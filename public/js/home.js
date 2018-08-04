741c7dbe9729f8ae08316df6c96e1ab5ee7c6290
hcoz <hcanozcelik@gmail.com>
2018-08-04 13:42:22 +0300

    sidebar fe optimization

--- public/js/home.js
+++ public/js/home.js
@@ -8,16 +8,19 @@ $(function () {
     }
   })
     .done(function (data) {
-      let userList = data.userList;
+      var userHtml = '';
 
-      for (let i in userList) {
-        $('.js-user-list').append('<li class="nav-item"><a class="nav-link js-list-user" href="#" data-username="' +
-          userList[i].userName +
-          (userList[i].isFollow ? '" data-following="true"><span data-feather="user-minus' : '" data-following="false"><span data-feather="user-plus') +
+      $.each(data.userList, function (i, user) {
+        userHtml += '<li class="nav-item' + (user.isFollow ? ' following' : '') +
+          '"><a class="nav-link js-list-user" href="#" data-username="' +
+          user.userName +
+          (user.isFollow ? '" data-following="true"><span data-feather="user-minus' : '" data-following="false"><span data-feather="user-plus') +
           '"></span>' +
-          userList[i].userName +
-          '</a></li>');
-      }
+          user.userName +
+          '</a></li>';
+      });
+
+      $('.js-user-list').append(userHtml);
       // initialize icons
       window.feather.replace();
     })
@@ -29,42 +32,25 @@ $(function () {
     });
 
   // fill post list
-  $.ajax({
-    type: 'GET',
-    url: '/api/postlist',
-    headers: {
-      'Authorization': window.localStorage.getItem('token')
-    }
-  })
-    .done(function (data) {
-      console.log(data);
-      var postHtml = '';
-
-      $.each(data.postList, function (i, post) {
-        postHtml += '<div class="post" data-postid=' + post._id +'><h4>' + post.postHeader + '</h4>' + 
-          '<h6><span data-feather="clock"></span> Post by ' + post.username + ', ' + new Date(post.date).toLocaleDateString() + '.</h6>' +
-          '<p>' + post.postContent + '</p></div>';
-      });
-
-      $('.js-post-list').append(postHtml);
-      // intialize icons
-      window.feather.replace();
-    })
-    .fail(function (err) {
-      console.error(err);
-
-      if (err.responseJSON.redirect)
-        window.location = err.responseJSON.redirect;
-    });
+  getPostList();
 
   // show/hide create post form
   $('.js-toggle-post-form').click(function () {
     $('.js-create-post').toggle();
     $(this).text(function (i, text) {
-      return text.trim() === 'New post' ? 'Hide' : 'New post';
+      return text.trim() === 'New Post' ? 'Hide' : 'New Post';
     });
   });
-  
+
+  // show/hide user list sidebar
+  $('.js-toggle-sidebar').click(function (e) {
+    e.preventDefault();
+    $('.js-sidebar').toggleClass('d-none');
+    $(this).text(function (i, text) {
+      return text.trim() === 'Show Users' ? 'Hide' : 'Show Users';
+    });
+  });
+
   // submit publish post form
   $('#publish').submit(function (e) {
     e.preventDefault();
@@ -105,6 +91,35 @@ $(function () {
   });
 });
 
+function getPostList() {
+  $.ajax({
+    type: 'GET',
+    url: '/api/postlist',
+    headers: {
+      'Authorization': window.localStorage.getItem('token')
+    }
+  })
+    .done(function (data) {
+      var postHtml = '';
+
+      $.each(data.postList, function (i, post) {
+        postHtml += '<div class="post" data-postid=' + post._id + '><h4>' + post.postHeader + '</h4>' +
+          '<h6><span data-feather="clock"></span> Post by ' + post.username + ', ' + new Date(post.date).toLocaleDateString() + '.</h6>' +
+          '<p>' + post.postContent + '</p></div>';
+      });
+
+      $('.js-post-list').empty().append(postHtml);
+      // intialize icons
+      window.feather.replace();
+    })
+    .fail(function (err) {
+      console.error(err);
+
+      if (err.responseJSON.redirect)
+        window.location = err.responseJSON.redirect;
+    });
+}
+
 function follow(userName) {
   $.ajax({
     type: 'POST',
@@ -115,12 +130,16 @@ function follow(userName) {
     data: { follow: userName }
   })
     .done(function (data) {
-      $('.js-user-list .js-list-user[data-username=' + data.userName + ']')
+      var $selectedUser = $('.js-user-list .js-list-user[data-username=' + data.userName + ']');
+      $selectedUser
         .data('following', data.isFollow)
         .empty()
         .append('<span data-feather="user-minus"></span>' + data.userName);
+      $selectedUser.parent().addClass('following');
       // intialize icons
       window.feather.replace();
+      // update post list
+      getPostList();
     })
     .fail(function (err) {
       console.error(err);
@@ -137,12 +156,16 @@ function unfollow(userName) {
     data: { unfollow: userName }
   })
     .done(function (data) {
-      $('.js-user-list .js-list-user[data-username=' + data.userName + ']')
+      var $selectedUser =  $('.js-user-list .js-list-user[data-username=' + data.userName + ']');
+      $selectedUser
         .data('following', data.isFollow)
         .empty()
         .append('<span data-feather="user-plus"></span>' + data.userName);
+      $selectedUser.parent().removeClass('following');
       // intialize icons
       window.feather.replace();
+      // update post list
+      getPostList();
     })
     .fail(function (err) {
       console.error(err);
